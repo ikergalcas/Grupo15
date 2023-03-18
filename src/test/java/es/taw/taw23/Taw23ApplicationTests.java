16a11550ad5f4d9cab318b67f84a833c8bd6bce2
Iker Gálvez Castillo <ikergalcas@gmail.com>
2023-03-18 19:05:48 +0100

    First commit

---
+++ src/test/java/es/taw/taw23/Taw23ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.taw.taw23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taw23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
