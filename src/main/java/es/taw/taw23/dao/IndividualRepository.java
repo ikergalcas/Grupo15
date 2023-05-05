b0fd07ac9bc62e7b23eda1267403a05471b2757f
PaprokaUMA <paproka@uma.es>
2023-05-05 06:32:24 +0200

    Subida final con las cosas limpitas

--- src/main/java/es/taw/taw23/dao/IndividualRepository.java
+++
@@ -1,8 +0,0 @@
-package es.taw.taw23.dao;
-
-import es.taw.taw23.entity.ClienteEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IndividualRepository extends JpaRepository<ClienteEntity,Integer> {
-
-}
